c12376634419b255a1841feea7ca3ae101e4a6ed
Justin Kumpe <jakumpe@kumpes.com>
2020-11-29 19:14:30 -0600

    Initial Commit

---
+++ RatingsRestrictionKit/RatingsRestrictionKit.h
@@ -0,0 +1,18 @@
+//
+//  RatingsRestrictionKit.h
+//  RatingsRestrictionKit
+//
+//  Created by Justin Kumpe on 11/29/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RatingsRestrictionKit.
+FOUNDATION_EXPORT double RatingsRestrictionKitVersionNumber;
+
+//! Project version string for RatingsRestrictionKit.
+FOUNDATION_EXPORT const unsigned char RatingsRestrictionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RatingsRestrictionKit/PublicHeader.h>
+
+
